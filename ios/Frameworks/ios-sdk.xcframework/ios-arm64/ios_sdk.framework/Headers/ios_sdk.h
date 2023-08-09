ac7a3251ffbd3b1a53a061f5e1a45b28e686ab4e
Dima Khort <dima@heycharge.com>
2023-08-09 17:23:27 +0200

    Fix no ios_sdk module found

---
+++ ios/Frameworks/ios-sdk.xcframework/ios-arm64/ios_sdk.framework/Headers/ios_sdk.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+//! Project version number for ios_sdk.
+FOUNDATION_EXPORT double ios_sdkVersionNumber;
+
+//! Project version string for ios_sdk.
+FOUNDATION_EXPORT const unsigned char ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_sdk/PublicHeader.h>
+
+
